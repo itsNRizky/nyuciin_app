4b7d3eee492ed01e242c9b4c9111947c74420d2a
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2022-10-19 23:06:02 +0700

    Add Signin and Dashboard Route

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import Homescreen from './screens/Homescreen';
 import Signin from './screens/Signin';
+import Signup from './screens/Signup';
+import Dashboard from './screens/Dashboard';
+
+
 
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Homescreen'>
-        <Stack.Screen name='Homescreen' component={Homescreen} options={{headerShown: false}} />
-        <Stack.Screen name='Signin' component={Signin} options={{headerShown: false, animation: 'slide_from_right'}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Homescreen'>
+          <Stack.Screen name='Homescreen' component={Homescreen} options={{headerShown: false}} />
+          <Stack.Screen name='Signin' component={Signin} options={{headerShown: false, animation: 'slide_from_right'}} />
+          <Stack.Screen name='Signup' component={Signup} options={{headerShown: false, animation: 'slide_from_right'}} />
+          <Stack.Screen name='Dashboard' component={Dashboard} options={{headerShown: false, animation: 'slide_from_bottom'}} />
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
